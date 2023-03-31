dda8c8179df837697f25dde5ffe211dde1eb0925
Oscar Quesada <51036668+ODQC@users.noreply.github.com>
2023-03-30 20:37:34 -0600

    GPS clase 03/30/2023

--- app/src/main/java/cr/ac/una/gps/MainActivity.kt
+++ app/src/main/java/cr/ac/una/gps/MainActivity.kt
@@ -2,10 +2,65 @@ package cr.ac.una.gps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+    lateinit var drawerLayout : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        var toogle = ActionBarDrawerToggle(
+
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        lateinit var fragment: Fragment
+        when (item.itemId) {
+            R.id.home -> {
+                fragment = HomeFragment.newInstance(param1 = "string1", param2 = "string2")
+
+            }
+            R.id.maps -> {
+                fragment = MapsFragment()
+            }
+        }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.home_content, fragment)
+                .commit()
+
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
